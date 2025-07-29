00118bd9e9d754d2abbe18271832edc52deed0f8
Ravindranath Tagore <rtravindra10@gmail.com>
2025-07-29 19:36:45 +0530

    Delete create-installer.bat

--- create-installer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Creating Windows installer (MSI)...
-echo This requires Java 14+ with jpackage tool
-
-jpackage --input . --name "SortingAlgoVisualizer" --main-jar SortingVisualizer.jar --main-class visualizer.SortingVisualizer --type msi --win-shortcut --win-menu
-
-echo Installer created! Check the current directory for SortingAlgoVisualizer.msi
-pause
